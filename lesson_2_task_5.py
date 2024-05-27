ef10cbb229eb2f4d82a6d64a90f217242f8d3841
Serg1818 <109500741+Serg1818@users.noreply.github.com>
2024-05-27 16:20:09 +0500

    Delete lesson_2_task_5.py

--- lesson_2_task_5.py
+++
@@ -1,8 +0,0 @@
-def month_to_season():
-   month = ['Зима', 'Весна', 'Лето', 'Осень'] 
-   a = int(input())
-   if 0 <= a <= 3:
-      print(month[a])
-   else:
-        print('Введите номер месяца в диапазоне 0 - 3')
-month_to_season()
